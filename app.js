300a7ba8da0f6a78d106dff9f7bc84e67182d2e2
whitemarble <wenhan@outlook.com>
2017-08-29 23:42:06 +1200

    koa router

--- app.js
+++ app.js
@@ -1,29 +1,10 @@
-const Koa = require('koa');
-const app = new Koa();
+const Koa = require('koa')
+const fs = require('fs')
+const app = new Koa()
+const router = require('./routers/index')
 
-// x-response-time
+app.use(router.routes()).use(router.allowedMethods())
 
-app.use(async (ctx, next) => {
-  const start = Date.now();
-  await next();
-  const ms = Date.now() - start;
-  ctx.set('X-Response-Time', `${ms}ms`);
-});
-
-// logger
-
-app.use(async (ctx, next) => {
-  const start = Date.now();
-  await next();
-  const ms = Date.now() - start;
-  console.log(`${ctx.method} ${ctx.url} - ${ms}`);
-});
-
-// response
-
-app.use(async ctx => {
-  ctx.body = 'Hello World';
-});
 
 const port = process.env.PORT || 443
 app.listen(port, () => console.log('Server listening on', port))
